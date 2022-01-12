f977fc1e9bc7313b2afc235af0794f64e63923dd
Luiz <ziul.bio@gmail.com>
2022-01-12 14:38:28 -0300

    ...

---
+++ bashScript.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Para terminar a execução do script se algum erro ocorrer
+set -o errexit
+
+conda activate <env_name>
+
+# ----------------------------------------------------------------------------------------------------
+
+#                                         Bash script exemple
+
+#-----------------------------------------------------------------------------------------------------                              
+
+
+# ------------------------------------------ Commands ------------------------------------------------
+
+# Teste a comando o script, se o script funcionar corretamente o output será a lista dos programas instalados no env ativado.
+conda list
+
